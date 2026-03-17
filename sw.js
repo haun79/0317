2acda580814cc22a396b69af6d585c360990015d
haun79 <a0906100268@gmail.com>
2026-03-17 14:50:44 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,16 @@
 const CACHE_NAME = 'soccer-game-v1';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './u_yl4sq8d6xg-ghibli-style-2-229070.mp3',
-  './peekaboolabcreative-11l-victory_sound_with_t-1749487409696-357609.mp3',
-  './icon-192.png',
-  './icon-512.png'
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'icon-512.png',
+  'u_yl4sq8d6xg-ghibli-style-2-229070.mp3',
+  'peekaboolabcreative-11l-victory_sound_with_t-1749487409696-357609.mp3'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
+      return cache.addAll(ASSETS);
     })
   );
 });
